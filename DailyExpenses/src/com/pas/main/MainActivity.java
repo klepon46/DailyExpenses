0929ee5e376fb4de5876a467f3f12905323bf4bb
Gusti Arya <klepon.boy14@gmail.com>
2013-07-04 22:27:38 +0800

    tambahan spinner(Combo box), Edit Text, dan Button.

--- DailyExpenses/src/com/pas/main/MainActivity.java
+++ DailyExpenses/src/com/pas/main/MainActivity.java
@@ -3,13 +3,30 @@ package com.pas.main;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private Button btnSimpan;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Spinner spinner = (Spinner) findViewById(R.id.exp_spinner);
+		ArrayAdapter<CharSequence> adapter = 
+				ArrayAdapter.createFromResource(this, 
+						R.array.expenses_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		btnSimpan = (Button) findViewById(R.id.btn_simpan);
+			
 	}
 
 	@Override
@@ -18,5 +35,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
