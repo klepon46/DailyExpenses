b5607f550eaf8941cb3053729692858cdbb80a3c
Gusti Arya <klepon.boy14@gmail.com>
2013-07-06 23:29:36 +0800

    COMMIT ARA
    - Add ReportActivity
    - Add OptionMenuitem
    - Add OptionMenuItemListener to open second Activyt (Report Activity)
    - Change application Icon

--- DailyExpenses/src/com/pas/main/MainActivity.java
+++ DailyExpenses/src/com/pas/main/MainActivity.java
@@ -2,7 +2,9 @@ package com.pas.main;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -36,5 +38,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menuBtnReport:
+			Intent intent = new Intent(this, ReportActivity.class);
+			startActivity(intent);
+			break;
+		}
+		return super.onMenuItemSelected(featureId, item);
 	}
 }
---
+++ DailyExpenses/src/com/pas/main/ReportActivity.java
@@ -0,0 +1,14 @@
+package com.pas.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReportActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_report);
+
+	}
+}
