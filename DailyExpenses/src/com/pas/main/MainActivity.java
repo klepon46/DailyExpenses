4b631aff0577db5dbe382bd52391e76758bc393f
Gusti Arya <klepon.boy14@gmail.com>
2013-07-24 15:37:07 +0800

    COMMIT ARA
    Tambahan di report activity

--- DailyExpenses/src/com/pas/main/MainActivity.java
+++ DailyExpenses/src/com/pas/main/MainActivity.java
@@ -1,14 +1,20 @@
 package com.pas.main;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
@@ -19,6 +25,9 @@ public class MainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 
 	private Button btnSimpan;
 	private Button btnReset;
+	private Spinner iSpinner;
+	private EditText iEditCost;
+	private EditText iEditDesc;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +40,46 @@ public class MainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 						R.array.expenses_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
+
+		iSpinner = (Spinner) findViewById(R.id.exp_spinner);
+		iEditCost = (EditText) findViewById(R.id.edit_cost);
+		iEditDesc = (EditText) findViewById(R.id.edit_desc);
+
+		addListnerButton();
+	}
+
+	public void addListnerButton(){
+		
 		btnSimpan = (Button) findViewById(R.id.btn_simpan);
 		btnReset = (Button) findViewById(R.id.btn_reset);
+
+		btnSimpan.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final String expCode = iSpinner.getSelectedItem().toString();
+				final String expDesc = iEditDesc.getText().toString();
+				final String expCost = iEditCost.getText().toString();
+				final BigDecimal exptCostInt = new BigDecimal(expCost);
+				
+				RuntimeExceptionDao<Expenses, Integer> expensesDao =
+						getHelper().getExpensesDao();
+				
+				Expenses exp = new Expenses(expCode, expDesc, new Date(), exptCostInt);
+				expensesDao.create(exp);
+				getHelper().close();
+				
+				Toast.makeText(getApplicationContext(), "Simpan Berhasil", 1).show();
+			}
+		});
+		
+		btnReset.setOnClickListener(new OnClickListener() {
 			
+			@Override
+			public void onClick(View arg0) {
+				
+			}
+		});
+
 	}
 
 	@Override
@@ -42,7 +88,7 @@ public class MainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
@@ -53,10 +99,10 @@ public class MainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 		}
 		return super.onMenuItemSelected(featureId, item);
 	}
-	
+
 	public void testDatabaseStuff(){
 		RuntimeExceptionDao<Expenses, Integer> expensesDao = getHelper().getExpensesDao();
 		List<Expenses> expList = expensesDao.queryForAll();
-		System.out.println(expList.size());
+
 	}
 }
--- DailyExpenses/src/com/pas/main/ReportActivity.java
+++ DailyExpenses/src/com/pas/main/ReportActivity.java
@@ -1,14 +1,48 @@
 package com.pas.main;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.dao.GenericRawResults;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.pas.bean.Expenses;
+import com.pas.databases.DatabaseHelper;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class ReportActivity extends Activity{
+public class ReportActivity extends OrmLiteBaseActivity<DatabaseHelper>{
 
+	private TextView tvTotal;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report);
-
+		populateFields();
+	}
+	
+	public void populateFields(){
+		String sum = "";
+		tvTotal = (TextView) findViewById(R.id.text_total);
+		
+		RuntimeExceptionDao<Expenses, Integer> expensesDao =
+				getHelper().getExpensesDao();
+		GenericRawResults<String[]> rawResults =
+				expensesDao.queryRaw("select sum(expensesAmount) from Expenses");
+		try {
+			List<String[]> results = rawResults.getResults();
+			for(String[] resultArray : results){
+				sum = resultArray[0];
+			}
+			tvTotal.setText(sum);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
